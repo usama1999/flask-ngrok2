12f7ced4a4a35b522d29c6b285d0292e4e711c46
Mohamed Rashad <m.rashadnow@gmail.com>
2022-08-24 00:15:25 +0200

    New version

--- flask_ngrok2.py
+++ flask_ngrok2.py
@@ -3,6 +3,7 @@ import json
 import os
 import platform
 import shutil
+import stat
 import subprocess
 import tempfile
 import time
@@ -31,15 +32,15 @@ def _run_ngrok(port):
     ngrok_path = str(Path(tempfile.gettempdir(), "ngrok"))
     _download_ngrok(ngrok_path)
     executable = str(Path(ngrok_path, command))
-    os.chmod(executable, 0o777)
-    ngrok = subprocess.Popen([executable, 'http', str(port)])
+    os.chmod(executable, stat.S_IEXEC)  # Make file executable for the current user.
+    ngrok = subprocess.Popen([executable, "http", str(port)])
     atexit.register(ngrok.terminate)
     localhost_url = "http://localhost:4040/api/tunnels"  # Url with tunnel details
     time.sleep(1)
     tunnel_url = requests.get(localhost_url).text  # Get the tunnel information
     j = json.loads(tunnel_url)
 
-    tunnel_url = j['tunnels'][0]['public_url']  # Do the parsing of the get
+    tunnel_url = j["tunnels"][0]["public_url"]  # Do the parsing of the get
     tunnel_url = tunnel_url.replace("https", "http")
     return tunnel_url
 
@@ -62,10 +63,10 @@ def _download_ngrok(ngrok_path):
 
 
 def _download_file(url):
-    local_filename = url.split('/')[-1]
+    local_filename = url.split("/")[-1]
     r = requests.get(url, stream=True)
     download_path = str(Path(tempfile.gettempdir(), local_filename))
-    with open(download_path, 'wb') as f:
+    with open(download_path, "wb") as f:
         shutil.copyfileobj(r.raw, f)
     return download_path
 
@@ -86,9 +87,10 @@ def run_with_ngrok(app):
     old_run = app.run
 
     def new_run(*args, **kwargs):
-        port = kwargs.get('port', 5000)
+        port = kwargs.get("port", 5000)
         thread = Timer(1, start_ngrok, args=(port,))
         thread.setDaemon(True)
         thread.start()
         old_run(*args, **kwargs)
+
     app.run = new_run
--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flask-ngrok",
-    version="0.0.26",
-    author="Grant Stafford",
-    description="A simple way to demo Flask apps from your machine.",
+    name="flask-ngrok2",
+    version="0.1.0",
+    author="Mohamed Rashad",
+    description="A successor to flask-ngrok for demo Flask apps using ngrok.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gstaff/flask-ngrok",
+    url="https://github.com/MohamedAliRashad/flask-ngrok2",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    keywords='flask ngrok demo',
+    keywords='flask ngrok2 demo',
     install_requires=['Flask>=0.8', 'requests'],
-    py_modules=['flask_ngrok']
+    py_modules=['flask_ngrok2']
 )
