44e729e839402da29c479fb3ad89be364946e1c7
Mohamed Rashad <m.rashadnow@gmail.com>
2022-08-24 01:43:32 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask-ngrok2",
-    version="0.1.0",
+    version="0.2.1",
     author="Mohamed Rashad",
     description="A successor to flask-ngrok for demo Flask apps using ngrok.",
     long_description=long_description,
